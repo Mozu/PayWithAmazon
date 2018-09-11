0d365f2776677665e8df745f11ffb842244801c8
thomas phipps <dirkelfman@gmail.com>
2018-09-11 15:23:40 -0500

    first bit of ecs

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
-const express             = require("express");
-const bodyParser          = require('body-parser');
-const cookieParser        = require('cookie-parser');
-const app                 = express();
-const tokenDetails        = require('./routes/tokendetails');
-const authorizewithtoken  = require('./routes/authorizewithtoken');
-const capture             = require('./routes/capture');
-const credit              = require('./routes/credit');
-const voidTran            = require('./routes/void');
-const validateToken       = require('./routes/validateToken');
-const helper              = require('./helper');
+const express = require("express");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const app = express();
+const tokenDetails = require('./routes/tokendetails');
+const authorizewithtoken = require('./routes/authorizewithtoken');
+const capture = require('./routes/capture');
+const credit = require('./routes/credit');
+const voidTran = require('./routes/void');
+const validateToken = require('./routes/validateToken');
+const helper = require('./helper');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
@@ -16,35 +16,40 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 
-app.use((req,res,next)=>{
-  const settings = req.body.context.settings;
-  const configuration = req.body.context.configuration;
+app.use((req, res, next) => {
 
-  let config = {
-    "mwsAccessKeyId" : helper.getValue(configuration, "mwsAccessKeyId"),
-    "mwsSecret" : helper.getValue(configuration, "mwsSecret"),
-    "sellerId" : helper.getValue(settings, "sellerId"),
-    "clientId" : helper.getValue(settings, "clientId"),
-    "environment" : helper.getValue(settings, "environment"),
-    "mwsAuthToken" : helper.getValue(settings, "mwsAuthToken"),
-    "awsRegion" : helper.getValue(settings, "awsRegion"),
-  
-  }
+    if (!req.body || !req.body.context) {
+        return next();
+    }
 
-  config.isSandbox = config.environment == "sandbox";
+    const settings = req.body.context.settings;
+    const configuration = req.body.context.configuration;
 
-  req.body.config = config;
+    let config = {
+        "mwsAccessKeyId": helper.getValue(configuration, "mwsAccessKeyId"),
+        "mwsSecret": helper.getValue(configuration, "mwsSecret"),
+        "sellerId": helper.getValue(settings, "sellerId"),
+        "clientId": helper.getValue(settings, "clientId"),
+        "environment": helper.getValue(settings, "environment"),
+        "mwsAuthToken": helper.getValue(settings, "mwsAuthToken"),
+        "awsRegion": helper.getValue(settings, "awsRegion"),
 
-  next();
+    }
+
+    config.isSandbox = config.environment == "sandbox";
+
+    req.body.config = config;
+
+    next();
 });
 
-app.use("/tokendetails",tokenDetails);  
+app.use("/tokendetails", tokenDetails);
 app.use("/authorizewithtoken", authorizewithtoken);
 app.use("/capture", capture);
 app.use("/credit", credit);
 app.use('/void', voidTran);
 app.use('/validateToken', validateToken);
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.send('Hello World!')
 });
 
